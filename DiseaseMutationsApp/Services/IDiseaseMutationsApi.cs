a1fc551aa2ce0c436d1eea38d2e0cc36b3948955
JavierStark <javiertorralbocortes@gmail.com>
2025-11-21 19:07:28 +0100

    feat: use refit to call backend api from page

---
+++ DiseaseMutationsApp/Services/IDiseaseMutationsApi.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+
+namespace DiseaseMutationsApp.Services;
+
+public interface IDiseaseMutationsApi
+{
+    [Get("/getbestrna")]
+    Task<List<Tuple<string,double,int,string>>> GetBestgRNA([Query] int window, [Query] string sequence);
+}
