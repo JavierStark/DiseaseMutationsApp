6e7646a82b303c0c5f5706b4ce862deaafb02655
JavierStark <javiertorralbocortes@gmail.com>
2025-11-21 19:07:28 +0100

    feat: use refit to call backend api from page

--- DiseaseMutationsApp/Program.cs
+++ DiseaseMutationsApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DiseaseMutationsApp;
+using DiseaseMutationsApp.Services;
+using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped(refit => RestService.For<IDiseaseMutationsApi>("https://localhost:5001"));
 await builder.Build().RunAsync();
