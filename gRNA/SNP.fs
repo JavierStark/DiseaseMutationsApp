2bff350aede3862cc5a1653c3ceb46a742ebb7f3
JavierStark <javiertorralbocortes@gmail.com>
2025-11-30 17:45:26 +0100

    feat: SNP to HGVS

---
+++ gRNA/SNP.fs
@@ -0,0 +1,37 @@
+﻿module gRNA.SNP
+
+open FSharp.Data.JsonProvider
+
+open System.Net.Http
+type SnpData = JsonProvider<"snp_sample.json">
+
+let private httpClient = new HttpClient()
+
+let loadJsonFromUrlAsync (url: string) : Async<string> =
+    async {
+        let! response = httpClient.GetStringAsync(url) |> Async.AwaitTask
+        return response
+    }
+
+let getHgvsNotationsAsync (rsNumber: int) : Async<string list> =
+    async {
+        let url = $"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rsNumber}"
+        
+        try
+            let! jsonString = loadJsonFromUrlAsync url
+            let snpData = SnpData.Parse(jsonString)
+            
+            let hgvsNotations =
+                snpData.PrimarySnapshotData.PlacementsWithAllele
+                |> Array.collect _.Alleles
+                |> Array.map _.Hgvs
+                |> Array.filter (_.StartsWith("NG_"))  // filter for genomic notations
+                |> Array.filter (fun h -> not (h.Contains("=")))  // exclude no-change notations
+                |> Array.toList
+            
+            return hgvsNotations
+            
+        with ex -> 
+            printfn "Error fetching SNP data: %s" ex.Message
+            return []
+    }
