ed4ceaae4dc7cd3e4b7e1deec1cc82fa01f590e8
JavierStark <javiertorralbocortes@gmail.com>
2025-12-11 10:51:44 +0100

    feat: keep state between pages

--- DiseaseMutationsApp/Program.cs
+++ DiseaseMutationsApp/Program.cs
@@ -10,4 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(refit => RestService.For<IDiseaseMutationsApi>("http://localhost:5000"));
+
+// Register state management service to persist data across page navigations
+builder.Services.AddScoped<AppStateService>();
+
 await builder.Build().RunAsync();
