f9ff75efacb670bfd6f59dd4f19ce87b29e99ac4
JavierStark <javiertorralbocortes@gmail.com>
2025-12-11 09:49:58 +0100

    feat: ui design improvement

--- gRNA/LibraryTesting.fsx
+++ gRNA/LibraryTesting.fsx
@@ -1,7 +1,23 @@
-﻿#r "nuget: FSharp.Data"
-#r "./bin/Debug/net9.0/gRNA.dll"
+﻿// Basic Option-style computation expression
+type OptionBuilder() =
+    member _.Bind(m, f) = Option.bind f m
+    member _.Return(x) = Some x
+    member _.ReturnFrom(m) = m
+    member _.Zero() = None
 
-open gRNA.Omim
+let option = OptionBuilder()
 
-rsFromOmim 261600
-|> printfn "%A"
+// Usage examples
+let addSome a b =
+    option {
+        let! x = a
+        let! y = b
+        return x + y
+    }
+
+let r1 = addSome (Some 2) (Some 3)    // Some 5
+let r2 = addSome None (Some 3)       // None
+
+// Print results (for script execution)
+printfn "r1 = %A" r1
+printfn "r2 = %A" r2
